4dc5576d7de875d2329a59b8967bf6c6387d0100
Luis Ballesteros <lballesteros@comercializadora-s3.com>
2024-02-05 17:58:55 -0500

    add partial conection with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'luis',
+      password: '123456',
+      database: 'Task',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      options: {
+        trustServerCertificate: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
